749ee0d5884816f026d27aacfb43d805f4acdc72
Yaël <yael.vincent@orange.fr>
2026-03-12 11:34:03 +0100

    Création du preview

--- app/src/main/java/com/example/ffsupercool/MainActivity.kt
+++ app/src/main/java/com/example/ffsupercool/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ffsupercool.ui.HomeScreen
 import com.example.ffsupercool.ui.theme.FFSUperCoolTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FFSUperCoolTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun AppPreview() {
     FFSUperCoolTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
